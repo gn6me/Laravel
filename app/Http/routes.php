ce9900be1b0e0818681deb6bf9fec66c7f4b5a97
Tim Sleeper <mrt.sleeper@gmail.com>
2015-10-22 12:15:29 -0400

    Created register and login views

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('welcome');
+});
+
 Route::get('users', function()
 {
-    return View::make('users');
+    $users = User::all();
+    
+    return View::make('users')->with('users', $users);
 });
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
